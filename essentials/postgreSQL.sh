cfc5b03d7339902700735ca3c3c6ff49bc2037c6
Silvio Schwarz <admin@silvioschwarz.com>
2022-04-06 14:09:05 +0200

    qgis and postgresql install script with architecture

--- essentials/postgreSQL.sh
+++ essentials/postgreSQL.sh
@@ -1,4 +1,15 @@
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb arch=$(dpkg --print-architecture)] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install postgresql
+
+
+
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/focal pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list' 
+
+sudo apt update
+sudo apt install pgadmin4
+
+
+sudo /usr/pgadmin4/bin/setup-web.sh 
--- essentials/qgis.sh
+++ essentials/qgis.sh
@@ -36,7 +36,7 @@ sudo apt install gnupg software-properties-common
 wget -qO - https://qgis.org/downloads/qgis-2021.gpg.key | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/qgis-archive.gpg --import
 sudo chmod a+r /etc/apt/trusted.gpg.d/qgis-archive.gpg
 
-sudo add-apt-repository "deb https://qgis.org/ubuntu $(lsb_release -c -s) main"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://qgis.org/ubuntu $(lsb_release -c -s) main"
 
 sudo apt update
 sudo apt install qgis qgis-plugin-grass python-qgis
